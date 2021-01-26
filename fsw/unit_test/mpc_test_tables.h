91793d8e8521e2932e85d51db52703e06b86fcc1
Mathew Benson <mbenson@windhoverlabs.com>
2021-01-25 19:08:30 -0600

    Squashed 'apps/mpc/' content from commit 64843b9
    git-subtree-dir: apps/mpc
    git-subtree-split: 64843b9b4bfb518b3f9802c6c202df2d549d573e

---
+++ fsw/unit_test/mpc_test_tables.h
@@ -0,0 +1,16 @@
+#ifndef MPC_TEST_TABLE_H
+#define MPC_TEST_TABLE_H
+
+#include "mpc_tbldefs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern MPC_ConfigTbl_t MPC_NominalConfigTbl;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
