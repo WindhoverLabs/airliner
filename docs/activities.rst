e36762ceb382439111f3d7647a8d552f43486d53
Mathew Benson <mbenson@windhoverlabs.com>
2021-01-25 19:08:53 -0600

    Squashed 'apps/prmlib/' content from commit 2deab04
    git-subtree-dir: apps/prmlib
    git-subtree-split: 2deab0432a27a25f869477cc4eeab8666abc298f

---
+++ docs/activities.rst
@@ -0,0 +1,9 @@
+Activities
+==========
+
+TODO: Add relevent PRMLIB Diagrams.
+
+Detailed design documentation is available `here <../../doxy/apps/prmlib/cfsprmlibovr.html>`_.
+
+.. image:: /docs/_static/doxygen.png
+   :target: ../../doxy/apps/prmlib/index.html  
---
+++ fsw/platform_inc/prmlib_msgids.h
@@ -0,0 +1,18 @@
+#ifndef PRMLIB_MSGIDS_H
+#define PRMLIB_MSGIDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PRMLIB_PARAM_UPDATED_MID                    0x0ca6
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PRMLIB_MSGIDS_H */
+
+/************************/
+/*  End of File Comment */
+/************************/
