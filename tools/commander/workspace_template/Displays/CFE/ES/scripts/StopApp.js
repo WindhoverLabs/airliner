d304906a0a509b6952e7a9328e43cb82b83e1e8d
Mathew Benson <mbenson@windhoverlabs.com>
2021-01-25 21:54:42 -0600

    Added basic look-and-feel of newly created displays in commander workspace

--- tools/commander/workspace_template/Displays/CFE/ES/scripts/StopApp.js
+++
@@ -1,6 +0,0 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-importPackage(Packages.org.yamcs.studio.script);
-
-var appName = display.getWidget("inAppName").getPropertyValue("text");
-
-Yamcs.issueCommand('/cfs/cfe_es/StopApp(Name: ' + appName + ')');
--- tools/commander/workspace_template/Displays/CFE/TIME/scripts/ResetCounters.js
+++
@@ -1,4 +0,0 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-importPackage(Packages.org.yamcs.studio.script);
-
-Yamcs.issueCommand('/cfs/cfe_time/Reset(arg: none)');
