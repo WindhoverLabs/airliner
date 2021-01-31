c2d0a9563e8247603b301f3bdf4d9fa1c357ad3f
Mathew Benson <mbenson@windhoverlabs.com>
2021-01-31 13:56:57 -0600

    Completed all CFE-ES displays. Use these as a template for all future displays.

--- core/base/tools/commander/workspace_template/Displays/Core/ES/scripts/SetLogSysMode.js
+++
@@ -1,7 +0,0 @@
-importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-importPackage(Packages.org.yamcs.studio.script);
-importPackage(Packages.org.yamcs.studio.data);
-
-var strMode =VTypeHelper.getString(display.getWidget('inMode').getPropertyValue('pv_value'));
-
-Yamcs.issueCommand('/cfs/cfe_es/SetSysLogMode', {'Payload.Mode': strMode});
