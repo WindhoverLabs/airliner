7d0b7cd624d2b09616aa4915cd96794070e7aef4
cnalley <cnalley@windhoverlabs.com>
2021-02-01 21:35:44 +0000

    Remove network includes from udp module.

--- apps/sbn/modules/udp/fsw/src/sbn_udp_if.h
+++ apps/sbn/modules/udp/fsw/src/sbn_udp_if.h
@@ -4,6 +4,14 @@
 #include "sbn_constants.h"
 #include "sbn_interfaces.h"
 #include "cfe.h"
+/* network includes */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
 
 /**
  * UDP-specific message types.
