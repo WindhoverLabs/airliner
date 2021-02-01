c1c0ac6a9cd30ca5175263ae4af3e325d0baca7b
cnalley <cnalley@windhoverlabs.com>
2021-02-01 18:03:56 +0000

    Add performance ids to shared folder.

--- config/shared/inc/sbn_perfids.h
+++ config/shared/inc/sbn_perfids.h
@@ -1,9 +1,6 @@
 #ifndef _sbn_perfids_h_
 #define _sbn_perfids_h_
 
-#define SBN_PERF_MIN_ID 1
-#define SBN_PERF_SEND_ID SBN_PERF_MIN_ID
-#define SBN_PERF_RECV_ID SBN_PERF_MIN_ID + 1
-#define SBN_PERF_MAX_ID SBN_PERF_MIN_ID + 100
+#include "perf_ids.h"
 
 #endif /* _sbn_perfids_h_ */
