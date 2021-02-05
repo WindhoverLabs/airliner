814dc693f950ea6a375e63387fc3715cc9eae489
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-02-05 17:36:32 -0600

    -CS has been verified

--- core/base/tools/commander/workspace_template/Displays/Apps/CS/scripts/EnableAll.js
+++ core/base/tools/commander/workspace_template/Displays/Apps/CS/scripts/EnableAll.js
@@ -1,4 +1,4 @@
 importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 importPackage(Packages.org.yamcs.studio.script);
 
-Yamcs.issueCommand('/CFS/CS/EnableAll(arg: none)');
+Yamcs.issueCommand('/cfs/cfs/EnableApps', {});
