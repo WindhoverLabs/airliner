b8e2cbb7a576949f06b4cc09a14b1e5d37d9ff5c
Mathew Benson <mbenson@windhoverlabs.com>
2021-02-14 15:57:41 -0600

    updated YAML file, and removed unused message IDs.

--- apps/ea/fsw/mission_inc/ea_msgids.h
+++ apps/ea/fsw/mission_inc/ea_msgids.h
@@ -4,9 +4,7 @@
 #define EA_CMD_MID                              0x1c29
 #define EA_SEND_HK_MID                          0x1c2a
 #define EA_WAKEUP_MID                           0x1c2c
-#define EA_PERFMON_MID            		0x1c2e
 
 #define EA_HK_TLM_MID                           0x0c2b
-#define EA_OUT_DATA_MID            		0x0c2d
 
 #endif
