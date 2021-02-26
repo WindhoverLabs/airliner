b4ac0d141861aec993115a21b5f7c48b57fabd77
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-02-26 17:12:28 -0600

    -Added command script for deleting and reset event filters

---
+++ core/base/tools/commander/workspace_template/Displays/Core/EVS/Scripts/Python/ResetEventFilter.py
@@ -0,0 +1,15 @@
+from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.yamcs.studio.data import VTypeHelper
+from org.yamcs.studio.script import Yamcs
+
+event_id = VTypeHelper.getString(widget.getPropertyValue("pv_value"))
+# NOTE: Not sure if using macros is a good idea when passing data between scripts as they are scoped to the OPI file
+# it looks like and not the OPI instance.
+app_name = display.getMacroValue("APP")
+
+Yamcs.issueCommand('/cfs/cfe_evs/ResetAppEventFilter',
+                   {
+                       'Payload.AppName': app_name,
+                       'Payload.EventID': event_id
+                   }
+                   )
