627dec47bb31a9753b9b83d246c3f6913c4270f7
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-01 16:15:26 -0600

    -Enforce all uppercase app names when sending event-related commands

--- core/base/tools/commander/workspace_template/Displays/Core/EVS/Scripts/Python/DeleteEventFilter.py
+++ core/base/tools/commander/workspace_template/Displays/Core/EVS/Scripts/Python/DeleteEventFilter.py
@@ -5,7 +5,7 @@ from org.yamcs.studio.script import Yamcs
 event_id = VTypeHelper.getString(widget.getPropertyValue("pv_value"))
 # NOTE: Not sure if using macros is a good idea when passing data between scripts as they are scoped to the OPI file
 # it looks like and not the OPI instance.
-app_name = display.getMacroValue("APP")
+app_name = display.getMacroValue("APP").upper()
 
 Yamcs.issueCommand('/cfs/cfe_evs/DeleteAppEventFilter',
                    {
