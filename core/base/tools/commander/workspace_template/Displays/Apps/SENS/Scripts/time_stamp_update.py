81d20bb0481d4100ca7d90a35659953139f8bfa1
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-02 17:22:16 -0600

    -Cleanup

--- core/base/tools/commander/workspace_template/Displays/Apps/SENS/Scripts/time_stamp_update.py
+++ core/base/tools/commander/workspace_template/Displays/Apps/SENS/Scripts/time_stamp_update.py
@@ -1,5 +1,4 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
-from org.yamcs.studio.data import VTypeHelper
 
 time = PVUtil.getTimeString(display.getWidget("Gyro_X").getPV(), "yyyy-MM-dd HH:mm:ss")
 
