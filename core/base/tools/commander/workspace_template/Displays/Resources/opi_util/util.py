ba45f26144b0705b9ab887aa45edfa51e1389321
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-10 11:52:22 -0600

    -Added validate_opi to util API

---
+++ core/base/tools/commander/workspace_template/Displays/Resources/opi_util/util.py
@@ -0,0 +1,23 @@
+"""
+utility API for OPI Script authors.
+"""
+
+def validate_opi(display):
+    """
+    Validate the OPI. Essentially check that we have all of the macros we need.
+
+    :return bool: True if this opi is valid. False otherwise.
+    """
+    is_valid = True
+
+    # getMacroValue returns a unicode object, NOT an str object. See "5.6.1. String Methods" of the python2.7.18 docs
+    # for details.
+    app_name = display.getMacroValue("APP")
+    project_name = display.getMacroValue("PROJECT_NAME")
+    yaml_path = display.getMacroValue("YAML_PATH")
+    template_opi = display.getMacroValue("TEMPLATE_OPI")
+
+    if app_name is None or project_name is None or yaml_path is None or template_opi is None:
+        is_valid = False
+
+    return is_valid
