5ed5c47946adc19735a67e2b7d690c84832c4766
cnalley <cnalley@windhoverlabs.com>
2021-03-19 16:29:02 +0000

    Formatting.

--- apps/sbn/modules/uio_mbox/fsw/src/sbn_uio_mailbox_if.h
+++ apps/sbn/modules/uio_mbox/fsw/src/sbn_uio_mailbox_if.h
@@ -20,20 +20,20 @@
 #define SBN_MAILBOX_BLOCKING_DELAY    (1)
 
 /* PQ specific */
-#define SBN_PQ_CHANNEL_NAME          ("MBOX")
-#define SBN_PQ_CHANNEL_NUMBER        (0)
-#define SBN_PQ_CONFIG_TABLENAME      ("CFG")
-#define SBN_PQ_DUMP_TABLENAME        ("DMP")
-#define SBN_PQ_CONFIG_TABLE_FILENAME ("/cf/apps/pq_cfg.tbl")
-#define SBN_PQ_CF_THROTTLE_SEM_NAME  ("PQ_CF_CH0_SEM")
-#define SBN_PQ_CF_SEM_INIT_VALUE     (1)
+#define SBN_PQ_CHANNEL_NAME           ("MBOX")
+#define SBN_PQ_CHANNEL_NUMBER         (0)
+#define SBN_PQ_CONFIG_TABLENAME       ("CFG")
+#define SBN_PQ_DUMP_TABLENAME         ("DMP")
+#define SBN_PQ_CONFIG_TABLE_FILENAME  ("/cf/apps/pq_cfg.tbl")
+#define SBN_PQ_CF_THROTTLE_SEM_NAME   ("PQ_CF_CH0_SEM")
+#define SBN_PQ_CF_SEM_INIT_VALUE      (1)
 
 /* Send task specific */
-#define SBN_PQ_SEND_TASK_NAME        ("PQ_OUTCH_0")
-#define SBN_PQ_SEND_TASK_STACK_SIZE  (131072)
-#define SBN_PQ_SEND_TASK_PRIORITY    (50)
-#define SBN_PQ_SEND_TASK_FLAGS       (OS_ENABLE_CORE_0)
-#define SBN_PQ_CHANNEL_GET_TIMEOUT   (500)
+#define SBN_PQ_SEND_TASK_NAME         ("PQ_OUTCH_0")
+#define SBN_PQ_SEND_TASK_STACK_SIZE   (131072)
+#define SBN_PQ_SEND_TASK_PRIORITY     (50)
+#define SBN_PQ_SEND_TASK_FLAGS        (OS_ENABLE_CORE_0)
+#define SBN_PQ_CHANNEL_GET_TIMEOUT    (500)
 
 
 typedef struct
