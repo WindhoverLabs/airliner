37edf38da7da4e77aaf1c3f743470df4ba0154ed
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-04-09 16:57:46 -0500

    -Added GPS(or custom epochs and CFS Epoch time patterns

--- core/base/tools/commander/workspace_template/Displays/Core/TIME/scripts/format_cfe_time.py
+++ core/base/tools/commander/workspace_template/Displays/Core/TIME/scripts/format_cfe_time.py
@@ -10,10 +10,33 @@
 """
 
 from com.windhoverlabs.studio.time import CFETime
+from com.windhoverlabs.studio.registry import YAMLRegistry
 
+import logging
 
 def main():
-    # FIXME: Add error checking. The pvs might not always be "there"(the user is not connected to the instance, etc)
-    widget.setPropertyValue('text', str(CFETime.getRelativeTime(pvs[1].getValue().getValue(), pvs[2].getValue().getValue())))
+    logging.basicConfig()
+    logger = logging.getLogger('TIME')
+
+    registry = YAMLRegistry()
+
+    # Don't specify an exception type on the except clause. If you do, Jython will not catch Java exceptions.
+    try:
+        display.getWidget("Met_Time_Field").setPropertyValue('text', str(CFETime.getRelativeTime(pvs[1].getValue().getValue(), pvs[2].getValue().getValue())))
+    except:
+        logger.error("Failed to fetch pvs for Met_Time_Field")
+    try:
+        display.getWidget("GPS_Time_Field").setPropertyValue('text', str(CFETime.getTimeSinceEpoch(pvs[1].getValue().getValue(), pvs[2].getValue().getValue(),
+                              registry.get(display.getMacroValue("REGISTRY_PATH") + "/config/CFE_TIME_EPOCH_YEAR/value"),
+                              registry.get(display.getMacroValue("REGISTRY_PATH") + "/config/CFE_TIME_EPOCH_DAY/value"),
+                              registry.get(display.getMacroValue("REGISTRY_PATH") + "/config/CFE_TIME_EPOCH_HOUR/value"),
+                              registry.get(display.getMacroValue("REGISTRY_PATH") + "/config/CFE_TIME_EPOCH_MINUTE/value"),
+                              registry.get(display.getMacroValue("REGISTRY_PATH") + "/config/CFE_TIME_EPOCH_SECOND/value"))))
+    except:
+        logger.error("Failed to fetch pvs for GPS_Time_Field")
+    try:
+        display.getWidget("CFS_Time_Field").setPropertyValue('text', str(CFETime.getTimeSinceCFSEpoch(pvs[1].getValue().getValue(), pvs[2].getValue().getValue())))
+    except:
+        logger.error("Failed to fetch pvs for CFS_Time_Field")
 
 main()
