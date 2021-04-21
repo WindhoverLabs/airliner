ffe4d69f2d0d78b1f34c9a200e56e2ddc71c80ff
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-04-21 16:07:57 -0500

    -Cleanup

--- core/base/tools/commander/workspace_template/Displays/Core/EVS/Scripts/Python/ResetEventFilter.py
+++ core/base/tools/commander/workspace_template/Displays/Core/EVS/Scripts/Python/ResetEventFilter.py
@@ -3,8 +3,6 @@ from org.yamcs.studio.data import VTypeHelper
 from org.yamcs.studio.script import Yamcs
 
 event_id = widget.getParent().getMacroValue("EVENTID")
-# NOTE: Not sure if using macros is a good idea when passing data between scripts as they are scoped to the OPI file
-# it looks like and not the OPI instance.
 app_name = display.getMacroValue("APP").upper()
 
 Yamcs.issueCommand('/cfs/'+display.getMacroValue('CPUID')+'/cfe_evs/ResetAppEventFilter',
