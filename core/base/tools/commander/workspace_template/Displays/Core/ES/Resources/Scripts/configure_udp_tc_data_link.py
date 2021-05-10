d53f2f7e75e23aa057eb3f276a1edea90b9dfe48
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-05-10 17:47:16 -0500

    -Added link argument to Configure Tc Data Link Display

--- core/base/tools/commander/workspace_template/Displays/Core/ES/Resources/Scripts/configure_udp_tc_data_link.py
+++ core/base/tools/commander/workspace_template/Displays/Core/ES/Resources/Scripts/configure_udp_tc_data_link.py
@@ -3,5 +3,6 @@ from com.windhoverlabs.studio.server.util import ServerUtil
 print('sending request to server...')
 port = display.getWidget("Port").getPropertyValue('pv_value').getValue()
 ip_address = display.getWidget("IP").getPropertyValue('pv_value').getValue()
+link_name = display.getWidget("Link").getPropertyValue('pv_value').getValue()
 
-ServerUtil.configureUpdTcDataLink(int(port), ip_address)
+ServerUtil.configureUpdTcDataLink(int(port), ip_address, link_name)
