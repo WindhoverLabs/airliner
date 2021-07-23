5c9c6fa581622d0d611b57c62ef6b7af297e302b
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-07-23 18:23:55 -0500

    -Cleanup

--- core/base/tools/commander/workspace_template/Displays/Apps/TO/Scripts/add_message_app.py
+++ core/base/tools/commander/workspace_template/Displays/Apps/TO/Scripts/add_message_app.py
@@ -17,20 +17,20 @@ msg_limit = None
 if not (display.getWidget('msgIdInput').getPropertyValue('pv_value') is None) and \
         not (display.getWidget('MsgLimit').getPropertyValue('pv_value') is None) and \
         not (display.getWidget('PQueueIdx').getPropertyValue('pv_value') is None) and \
-		not (display.getWidget('ChannelIndex').getPropertyValue('pv_value') is None):
-	msg_key = display.getWidget('msgIdInput').getPropertyValue('pv_value').getValue()
-	msg_limit = int(display.getWidget('MsgLimit').getPropertyValue('pv_value').getValue())
-	pq_index = int(display.getWidget('PQueueIdx').getPropertyValue('pv_value').getValue())
-	channel_index = int(display.getWidget('ChannelIndex').getPropertyValue('pv_value').getValue())
+        not (display.getWidget('ChannelIndex').getPropertyValue('pv_value') is None):
+    msg_key = display.getWidget('msgIdInput').getPropertyValue('pv_value').getValue()
+    msg_limit = int(display.getWidget('MsgLimit').getPropertyValue('pv_value').getValue())
+    pq_index = int(display.getWidget('PQueueIdx').getPropertyValue('pv_value').getValue())
+    channel_index = int(display.getWidget('ChannelIndex').getPropertyValue('pv_value').getValue())
 
-	# NOTE: These might be redundant...
-	if msg_key in all_tlm:
-		msgID = all_tlm[msg_key]["msgID"]
+    # NOTE: These might be redundant...
+    if msg_key in all_tlm:
+        msgID = all_tlm[msg_key]["msgID"]
 
-		Yamcs.issueCommand('/cfs/' + display.getMacroValue('CPUID') + '/to/AddMessageFlow',
-                           {	'ChannelIdx': channel_index,
-							   	'MsgID': msgID,
-                               	'MsgLimit': msg_limit,
-								'PQueueIdx': pq_index
+        Yamcs.issueCommand('/cfs/' + display.getMacroValue('CPUID') + '/to/AddMessageFlow',
+                           {'ChannelIdx': channel_index,
+                            'MsgID': msgID,
+                            'MsgLimit': msg_limit,
+                            'PQueueIdx': pq_index
                             }
                            )
