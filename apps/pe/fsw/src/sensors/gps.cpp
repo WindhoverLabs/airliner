109fc5c2c4d51d11269da7917ef293a34dd1e593
Casey Nalley <cnalley@windhoverlabs.com>
2021-08-03 13:42:27 -0500

    Copy GPS beta to HK

--- apps/pe/fsw/src/sensors/gps.cpp
+++ apps/pe/fsw/src/sensors/gps.cpp
@@ -235,6 +235,9 @@ void PE::gpsCorrect()
      **/ 
     m_GPS.beta_thresh = 1e2f;
 
+    /* Save GPS beta for HK */
+    HkTlm.GpsBeta = m_GPS.beta;
+
     if (m_GPS.beta / BETA_TABLE[n_y_gps] > m_GPS.beta_thresh) 
     {
         if (!HkTlm.GpsFault)
