741ce5498e250f6c895b7ac69bbdbb767dde46db
Shahed Rahim <srahim@windhoverlabs.com>
2022-07-13 12:35:36 -0500

    csv working

--- core/base/tools/commander/params_to_csv.py
+++ core/base/tools/commander/params_to_csv.py
@@ -1,57 +1,39 @@
 import argparse
 import requests
-import urllib.parse
-
-# from yamcs.client import YamcsClient
+import yaml
 
 
-def yamcs_rcv(instance, p1, start, stop):
-    # client = YamcsClient('localhost:8090')
-    uri = 'http://localhost:8090/api/archive/' + instance + ":exportParameterValues"
-    param_dict = {'parameters': p1, 'start': start, 'stop': stop, 'delimiter': 'COMMA'}
-    uri_str = uri + "?" + urllib.parse.urlencode(param_dict)
-    print(uri_str)
+def read_yaml(yaml_file: str) -> dict:
+    yaml_data = yaml.load(open(yaml_file, 'r'),
+                          Loader=yaml.FullLoader)
+    return yaml_data
 
-    print(uri)
+
+def yamcs_rcv(yaml_dict):
+    uri = yaml_dict['uri'] + yaml_dict['instance'] + ":" + yaml_dict['method']
+    param_dict = {'parameters': yaml_dict['param_array'], 'start': yaml_dict['start'], 'stop': yaml_dict['stop'],
+                  'delimiter': 'COMMA'}
     s = requests.session()
 
-    # with s.get(uri, params=param_dict, stream=True) as resp:
-    with s.get(uri_str, params=None, stream=True) as resp:
-        for line in resp.iter_lines():
-            if line:
-                print(line)
-
-    # client = YamcsClient(uri)
-    # mdb = client.get_mdb(instance='simulator')
-    # # ...
-    # archive = client.get_archive(instance='simulator')
-    # # ...
-    # processor = client.get_processor(instance='simulator', processor='realtime')
-    # ...
+    with s.get(uri, params=param_dict, stream=True) as resp:
+        with open(yaml_dict['output_csv_file'], 'wb') as csvfile:
+            for line in resp.iter_lines():
+                if line:
+                    csvfile.write(line + b'\n')
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--instance", help="yamcs instance",
-                        type=str)
-
-    parser.add_argument("--p1", help="yamcs param path",
-                        type=str)
-
-    parser.add_argument("--start", help="yamcs start time UTC",
-                        type=str)
-
-    parser.add_argument("--stop", help="yamcs stop time UTC",
+    parser.add_argument("--file", help="yaml configuration file",
                         type=str)
 
     args = parser.parse_args()
-    print(args.instance)
-    print(args.p1)
-    print(args.start)
-    print(args.stop)
+    print(args.file)
 
-    # yamcs_rcv()
-    yamcs_rcv(args.instance, args.p1, args.start, args.stop)
+    yaml_dict = read_yaml(args.file)
+    print(yaml_dict)
+
+    yamcs_rcv(yaml_dict)
 
 
 main()
