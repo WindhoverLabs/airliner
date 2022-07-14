11c329e2394d0c90e4574aa3fb989d5697b7f026
Shahed Rahim <srahim@windhoverlabs.com>
2022-07-14 14:56:09 -0500

    diff time in ms working

--- core/base/tools/commander/params_to_csv.py
+++ core/base/tools/commander/params_to_csv.py
@@ -1,6 +1,11 @@
 import argparse
+import csv
+
 import requests
 import yaml
+
+import pandas as pd
+import datetime as dt
 
 
 def read_yaml(yaml_file: str) -> dict:
@@ -9,17 +14,34 @@ def read_yaml(yaml_file: str) -> dict:
     return yaml_data
 
 
-def yamcs_rcv(yaml_dict):
+def rcv_csv_from_yamcs(yaml_dict):
     uri = yaml_dict['uri'] + yaml_dict['instance'] + ":" + yaml_dict['method']
     param_dict = {'parameters': yaml_dict['param_array'], 'start': yaml_dict['start'], 'stop': yaml_dict['stop'],
                   'delimiter': 'COMMA'}
     s = requests.session()
 
     with s.get(uri, params=param_dict, stream=True) as resp:
-        with open(yaml_dict['output_csv_file'], 'wb') as csvfile:
+        with open(yaml_dict['yamcs_csv_file'], 'wb') as csvfile:
             for line in resp.iter_lines():
                 if line:
                     csvfile.write(line + b'\n')
+
+
+def parse_csv(yaml_dict):
+    csv_data = pd.read_csv(yaml_dict['yamcs_csv_file'])
+    time_col = csv_data['Time']
+    epoch_ms_delta_col = []
+    time_cell_count = 0
+    for time_cell in time_col:
+        dtime = dt.datetime.strptime(time_cell, "%Y-%m-%dT%H:%M:%S.%fZ")
+        if time_cell_count == 0:
+            dt_zero = dtime
+        dt_delta_ms = (dtime - dt_zero).total_seconds()*1000
+        epoch_ms_delta_col.insert(time_cell_count, dt_delta_ms)
+
+        time_cell_count += 1
+
+    print(epoch_ms_delta_col)
 
 
 def main():
@@ -33,7 +55,8 @@ def main():
     yaml_dict = read_yaml(args.file)
     print(yaml_dict)
 
-    yamcs_rcv(yaml_dict)
+    rcv_csv_from_yamcs(yaml_dict)
+    parse_csv(yaml_dict)
 
 
 main()
