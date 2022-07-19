3786d460038a137dbff7f2b9a825ecd54c443ffd
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2022-07-19 14:01:09 -0500

    -Add generate_coverage

---
+++ core/base/tools/ci/generate_coverage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Example: software/airliner/public/core/base/tools/ci/generate_coverage.sh software/airliner/public/build/fixedwing/gemini2/cpd/sitl/target
+DIR="$1"
+lcov -c -i -b $DIR -d $DIR -o Coverage.baseline
+lcov -c -d $DIR -b $DIR -o Coverage.out
+lcov -a Coverage.baseline -a Coverage.out -o Coverage.combined
+lcov -r Coverage.combined /usr/include/\* -o Coverage.combined
+genhtml Coverage.combined -o HTML
