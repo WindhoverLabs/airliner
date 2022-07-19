c8dae05f19db88fcac0a9acfe884d6441a9ee5f7
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2022-07-19 18:47:03 -0500

    -Output txt files from generate_coverage for CI.

--- core/base/tools/ci/generate_coverage.sh
+++ core/base/tools/ci/generate_coverage.sh
@@ -3,6 +3,6 @@
 DIR="$1"
 lcov -c -i -b $DIR -d $DIR -o Coverage.baseline
 lcov -c -d $DIR -b $DIR -o Coverage.out
-lcov -a Coverage.baseline -a Coverage.out -o Coverage.combined
-lcov -r Coverage.combined /usr/include/\* -o Coverage.combined
-genhtml Coverage.combined -o HTML
+lcov -a Coverage.baseline -a Coverage.out -o Coverage.txt
+lcov -r Coverage.txt /usr/include/\* -o Coverage.txt
+genhtml Coverage.txt -o HTML
