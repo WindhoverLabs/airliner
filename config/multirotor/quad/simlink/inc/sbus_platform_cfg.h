c83a763451275ba65ef861d0da6f0c9a8f16e02a
Casey Nalley <cnalley@windhoverlabs.com>
2022-08-01 10:31:28 -0500

    Update sbus_platform_cfg.h.

--- config/multirotor/quad/simlink/inc/sbus_platform_cfg.h
+++ config/multirotor/quad/simlink/inc/sbus_platform_cfg.h
@@ -101,7 +101,7 @@ extern "C" {
 **       The length of each string, including the NULL terminator cannot exceed
 **       the #OS_MAX_PATH_LEN value.
 */
-#define SBUS_CONFIG_TABLE_FILENAME      ("/cf/apps/sbus_config.tbl")
+#define SBUS_CONFIG_TABLE_FILENAME      ("/cf/apps/sbus_cfg.tbl")
 /** \brief The timeout value, in milliseconds, to wait for ES application startup sync.
 **
 **  \par Limits:
