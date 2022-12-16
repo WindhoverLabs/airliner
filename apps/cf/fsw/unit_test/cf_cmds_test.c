de2c1afd19d6819b55e589c0809031d7ec5207c2
Young Nielson <ynielson@windhoverlabs.com>
2022-12-16 14:58:38 -0600

    Updated CF unit test(updated cmds test cases)

--- apps/cf/fsw/unit_test/cf_cmds_test.c
+++ apps/cf/fsw/unit_test/cf_cmds_test.c
@@ -35,6 +35,7 @@
 #include "cf_test_utils.h"
 
 #include "cf_events.h"
+#include "cf_version.h"
 
 #include "uttest.h"
 #include "ut_osapi_stubs.h"
@@ -59,27 +60,402 @@
  */
 void Test_CF_AppPipe_InvalidCmdMessage(void)
 {
-    int32           CmdPipe;
     CF_NoArgsCmd_t  CmdMsg;
     char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
 
-    /* The following will emulate the behavior of receiving a message,
-       and gives it data to process. */
-    CmdPipe = Ut_CFE_SB_CreatePipe(CF_PIPE_NAME);
     CFE_SB_InitMsg((void*)&CmdMsg, 0, sizeof(CmdMsg), TRUE);
-    Ut_CFE_SB_AddMsgToPipe((void*)&CmdMsg, (CFE_SB_PipeId_t)CmdPipe);
     CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
 
-    Ut_CFE_ES_SetReturnCode(UT_CFE_ES_RUNLOOP_INDEX, FALSE, 2);
-
     /* Execute the function being tested */
-    CF_AppMain();
+    CF_AppInit();
+
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
 
     sprintf(expEvent, "Unexpected Msg Received MsgId -- ID = 0x%04X", 0);
 
     /* Verify results */
     UtAssert_EventSent(CF_MID_ERR_EID, CFE_EVS_ERROR, expEvent,
                        "CF_AppMain, fail InvalidCmdMessage: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, InvalidCmdCode
+ */
+void Test_CF_AppPipe_InvalidCmdCode(void)
+{
+    CF_NoArgsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg), TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)100);
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppInit();
+
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent,
+        "Cmd Msg with Invalid command code Rcvd -- ID = 0x%04X, CC = %d",
+        CF_CMD_MID, 100);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 0) &&
+                  (CF_AppData.Hk.ErrCounter == 1),
+                  "Test_CF_AppPipe_InvalidCmdCode");
+
+    UtAssert_EventSent(CF_CC_ERR_EID, CFE_EVS_ERROR, expEvent,
+                       "Test_CF_AppPipe_InvalidCmdCode: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, NoopCmd
+ */
+void Test_CF_AppPipe_NoopCmd(void)
+{
+    CF_NoArgsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg), TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_NOOP_CC);
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppInit();
+
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "CF No-op command, Version %d.%d.%d.%d",
+                      CF_MAJOR_VERSION, CF_MINOR_VERSION,
+                      CF_REVISION, CF_MISSION_REV);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 1) &&
+                  (CF_AppData.Hk.ErrCounter == 0),
+                  "CF_AppPipe, NoopCmd");
+
+    UtAssert_EventSent(CF_NOOP_CMD_EID, CFE_EVS_INFORMATION, expEvent,
+                       "CF_AppPipe, NoopCmd: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, NoopCmdInvLen
+ */
+void Test_CF_AppPipe_NoopCmdInvLen(void)
+{
+    CF_NoArgsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg) + 5, TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_NOOP_CC);
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "Cmd Msg with Bad length Rcvd: ID = 0x%X, CC = %d, "
+            "Exp Len = %d, Len = %d",
+            CF_CMD_MID, CF_NOOP_CC, sizeof(CmdMsg), sizeof(CmdMsg) + 5);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 0) &&
+                  (CF_AppData.Hk.ErrCounter == 1),
+                  "CF_AppPipe, NoopCmdInvLen");
+
+    UtAssert_EventSent(CF_CMD_LEN_ERR_EID, CFE_EVS_ERROR, expEvent,
+                       "CF_AppPipe, NoopCmdInvLen: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, RstCtrsCmd
+ */
+void Test_CF_AppPipe_RstCtrsCmd(void)
+{
+    CF_ResetCtrsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg), TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_RESET_CC);
+    CmdMsg.Value = 0;
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppInit();
+
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "Reset Counters command received - Value %u",
+            CmdMsg.Value);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 0) &&
+                  (CF_AppData.Hk.ErrCounter == 0),
+                  "CF_AppPipe, RstCtrsCmd");
+
+    UtAssert_EventSent(CF_RESET_CMD_EID, CFE_EVS_DEBUG, expEvent,
+                       "CF_AppPipe, RstCtrsCmd: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, RstCtrsCmdInvLen
+ */
+void Test_CF_AppPipe_RstCtrsCmdInvLen(void)
+{
+    CF_ResetCtrsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg) + 5, TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_RESET_CC);
+    CmdMsg.Value = 0;
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "Cmd Msg with Bad length Rcvd: ID = 0x%X, CC = %d, "
+            "Exp Len = %d, Len = %d",
+            CF_CMD_MID, CF_RESET_CC, sizeof(CmdMsg), sizeof(CmdMsg) + 5);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 0) &&
+                  (CF_AppData.Hk.ErrCounter == 1),
+                  "CF_AppPipe, RstCtrsCmdInvLen");
+
+    UtAssert_EventSent(CF_CMD_LEN_ERR_EID, CFE_EVS_ERROR, expEvent,
+                       "CF_AppPipe, RstCtrsCmdInvLen: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, FreezeCmd
+ */
+void Test_CF_AppPipe_FreezeCmd(void)
+{
+    CF_NoArgsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg), TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_FREEZE_CC);
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppInit();
+
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "%s", "Freeze command received.");
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 1) &&
+                  (CF_AppData.Hk.ErrCounter == 0),
+                  "CF_AppPipe, FreezeCmd");
+
+    UtAssert_True(CF_AppData.Hk.Eng.Flags == 1,
+                  "CF_AppPipe, FreezeCmd: Flags set");
+
+    UtAssert_EventSent(CF_FREEZE_CMD_EID, CFE_EVS_INFORMATION, expEvent,
+                       "CF_AppPipe, FreezeCmd: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, FreezeCmdInvLen
+ */
+void Test_CF_AppPipe_FreezeCmdInvLen(void)
+{
+    CF_NoArgsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg) + 5, TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_FREEZE_CC);
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "Cmd Msg with Bad length Rcvd: ID = 0x%X, CC = %d, "
+            "Exp Len = %d, Len = %d",
+            CF_CMD_MID, CF_FREEZE_CC, sizeof(CmdMsg), sizeof(CmdMsg) + 5);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 0) &&
+                  (CF_AppData.Hk.ErrCounter == 1),
+                  "CF_AppPipe, FreezeCmdInvLen");
+
+    UtAssert_EventSent(CF_CMD_LEN_ERR_EID, CFE_EVS_ERROR, expEvent,
+                       "CF_AppPipe, FreezeCmdInvLen: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, ThawCmd
+ */
+void Test_CF_AppPipe_ThawCmd(void)
+{
+    CF_NoArgsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg), TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_THAW_CC);
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppInit();
+
+    CF_AppData.Hk.Eng.Flags = 1;
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "%s", "Thaw command received.");
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 1) &&
+                  (CF_AppData.Hk.ErrCounter == 0),
+                  "CF_AppPipe, ThawCmd");
+
+    UtAssert_True(CF_AppData.Hk.Eng.Flags == 0,
+                  "CF_AppPipe, ThawCmd: Flags cleared");
+
+    UtAssert_EventSent(CF_THAW_CMD_EID, CFE_EVS_INFORMATION, expEvent,
+                       "CF_AppPipe, ThawCmd: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, ThawCmdInvLen
+ */
+void Test_CF_AppPipe_ThawCmdInvLen(void)
+{
+    CF_NoArgsCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg) + 5, TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_THAW_CC);
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "Cmd Msg with Bad length Rcvd: ID = 0x%X, CC = %d, "
+            "Exp Len = %d, Len = %d",
+            CF_CMD_MID, CF_THAW_CC, sizeof(CmdMsg), sizeof(CmdMsg) + 5);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 0) &&
+                  (CF_AppData.Hk.ErrCounter == 1),
+                  "CF_AppPipe, ThawCmdInvLen");
+
+    UtAssert_EventSent(CF_CMD_LEN_ERR_EID, CFE_EVS_ERROR, expEvent,
+                       "CF_AppPipe, ThawCmdInvLen: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, SuspendTransIdCmd
+ */
+void Test_CF_AppPipe_SuspendTransIdCmd(void)
+{
+    CF_CARSCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg), TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_SUSPEND_CC);
+    strcpy(CmdMsg.Trans, "0.3_1");
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppInit();
+
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "%s command received.%s", "Suspend", CmdMsg.Trans);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 1) &&
+                  (CF_AppData.Hk.ErrCounter == 0),
+                  "CF_AppPipe, SuspendTransIdCmd");
+
+    UtAssert_EventSent(CF_CARS_CMD_EID, CFE_EVS_INFORMATION, expEvent,
+                       "CF_AppPipe, SuspendTransIdCmd: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, SuspendFilenameCmd
+ */
+void Test_CF_AppPipe_SuspendFilenameCmd(void)
+{
+    CF_CARSCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg), TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_SUSPEND_CC);
+//    strcpy(CmdMsg.Trans, "/cf/file.txt");
+    strcpy(CmdMsg.Trans, "/ram/downlink/cpd_to_gnd/class1/priority2/");
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppInit();
+
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "%s command received.%s", "Suspend", CmdMsg.Trans);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 1) &&
+                  (CF_AppData.Hk.ErrCounter == 0),
+                  "CF_AppPipe, SuspendFilenameCmd");
+
+    UtAssert_EventSent(CF_CARS_CMD_EID, CFE_EVS_INFORMATION, expEvent,
+                       "CF_AppPipe, SuspendFilenameCmd: Event Sent");
+}
+
+
+/**
+ * Test CF_AppPipe, SuspendTransIdCmdInvLen
+ */
+void Test_CF_AppPipe_SuspendTransIdCmdInvLen(void)
+{
+    CF_CARSCmd_t  CmdMsg;
+    char  expEvent[CFE_EVS_MAX_MESSAGE_LENGTH];
+
+    CFE_SB_InitMsg((void*)&CmdMsg, CF_CMD_MID, sizeof(CmdMsg) + 5, TRUE);
+    CFE_SB_SetCmdCode((CFE_SB_MsgPtr_t)&CmdMsg, (uint16)CF_SUSPEND_CC);
+    strcpy(CmdMsg.Trans, "0.3_1");
+    CF_Test_PrintCmdMsg((void*)&CmdMsg, sizeof(CmdMsg));
+
+    /* Execute the function being tested */
+    CF_AppData.MsgPtr = (CFE_SB_MsgPtr_t)&CmdMsg;
+    CF_AppPipe(CF_AppData.MsgPtr);
+
+    sprintf(expEvent, "Cmd Msg with Bad length Rcvd: ID = 0x%X, CC = %d, "
+            "Exp Len = %d, Len = %d",
+            CF_CMD_MID, CF_SUSPEND_CC, sizeof(CmdMsg), sizeof(CmdMsg) + 5);
+
+    /* Verify results */
+    UtAssert_True((CF_AppData.Hk.CmdCounter == 0) &&
+                  (CF_AppData.Hk.ErrCounter == 1),
+                  "CF_AppPipe, SuspendTransIdCmdInvLen");
+
+    UtAssert_EventSent(CF_CMD_LEN_ERR_EID, CFE_EVS_ERROR, expEvent,
+                       "CF_AppPipe, SuspendTransIdCmdInvLen: Event Sent");
 }
 
 
@@ -92,4 +468,40 @@ void CF_Cmds_Test_AddTestCases(void)
     UtTest_Add(Test_CF_AppPipe_InvalidCmdMessage,
                CF_Test_Setup, CF_Test_TearDown,
                "Test_CF_AppPipe_InvalidCmdMessage");
+    UtTest_Add(Test_CF_AppPipe_InvalidCmdCode,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_InvalidCmdCode");
+    UtTest_Add(Test_CF_AppPipe_NoopCmd,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_NoopCmd");
+    UtTest_Add(Test_CF_AppPipe_NoopCmdInvLen,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_NoopCmdInvLen");
+    UtTest_Add(Test_CF_AppPipe_RstCtrsCmd,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_RstCtrsCmd");
+    UtTest_Add(Test_CF_AppPipe_RstCtrsCmdInvLen,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_RstCtrsCmdInvLen");
+    UtTest_Add(Test_CF_AppPipe_FreezeCmd,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_FreezeCmd");
+    UtTest_Add(Test_CF_AppPipe_FreezeCmdInvLen,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_FreezeCmdInvLen");
+    UtTest_Add(Test_CF_AppPipe_ThawCmd,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_ThawCmd");
+    UtTest_Add(Test_CF_AppPipe_ThawCmdInvLen,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_ThawCmdInvLen");
+    UtTest_Add(Test_CF_AppPipe_SuspendTransIdCmd,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_SuspendTransIdCmd");
+    UtTest_Add(Test_CF_AppPipe_SuspendTransIdCmdInvLen,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_SuspendTransIdCmdInvLen");
+    UtTest_Add(Test_CF_AppPipe_SuspendFilenameCmd,
+               CF_Test_Setup, CF_Test_TearDown,
+               "Test_CF_AppPipe_SuspendFilenameCmd");
 }
